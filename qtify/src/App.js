eae0b74f99f8564d74e54c41ea2ec78bd60d6745
Rahul Dubey <rahul.dubey.kr@gmail.com>
2024-06-21 11:53:24 +0530

    C4: Search Bar

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button/Button';
+import Searchbar from './components/Searchbar/Searchbar';
 
 function App() {
   return (
     <div className="App">
       <Button text={"Give Feedback"}/>
-      
+      <Searchbar/>
     </div>
   );
 }
--- qtify/src/components/Searchbar/Searchbar.jsx
+++ qtify/src/components/Searchbar/Searchbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import search from "../../assets/Search icon.svg";
+import './Searchbar.css'
 
 const Searchbar = ({placeholder}) => {
   return (
     <div className="search" >
       <input type="text" placeholder={placeholder}></input>
+      <button><img src={search} alt='searchImg'/></button>
     </div>
   );
 }
