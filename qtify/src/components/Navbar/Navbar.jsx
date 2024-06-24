295c77472168d06e5d0ca0a467fcf667712d0ee1
Rahul Dubey <rahul.dubey.kr@gmail.com>
2024-06-24 11:15:41 +0530

    Navbar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Button from '../Button/Button'
 import Searchbar from '../Searchbar/Searchbar'
-import logo from '../../assets/logo.png'
+import Logo from '../Logo/Logo'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={logo} alt="Logo" className='logo' width={67} height={34}/>
+        <Logo/>
         <Searchbar placeholder={"Search a song of your choice"}/>
         <Button text={"Give Feedback"}/>
     </div>
